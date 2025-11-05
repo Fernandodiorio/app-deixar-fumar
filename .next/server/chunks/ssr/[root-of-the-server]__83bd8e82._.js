10954bb6df2b6501189a12bcf4406e8edbe8f271
Fernando Monteiro <fernando@exemplo.com>
2025-11-05 09:45:38 +0000

    Tudo

--- .next/server/chunks/ssr/[root-of-the-server]__83bd8e82._.js
+++ .next/server/chunks/ssr/[root-of-the-server]__83bd8e82._.js
@@ -124,7 +124,6 @@ const metadata = {
 function RootLayout({ children }) {
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$Desktop$2f$PASTAS$2f$RESPIRAPT$2f$app$2d$deixar$2d$fumar$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("html", {
         lang: "pt-BR",
-        suppressHydrationWarning: true,
         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$Desktop$2f$PASTAS$2f$RESPIRAPT$2f$app$2d$deixar$2d$fumar$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("body", {
             className: `${__TURBOPACK__imported__module__$5b$next$5d2f$internal$2f$font$2f$google$2f$inter_c15e96cb$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"].variable} font-inter antialiased`,
             children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$Desktop$2f$PASTAS$2f$RESPIRAPT$2f$app$2d$deixar$2d$fumar$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$Desktop$2f$PASTAS$2f$RESPIRAPT$2f$app$2d$deixar$2d$fumar$2f$src$2f$components$2f$ui$2f$theme$2d$provider$2e$tsx__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["ThemeProvider"], {
